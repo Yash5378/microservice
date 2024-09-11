1ae20e231958d9c2a25261bd9ceec18f6803a9de
Yash5378 <yashkumar52622@gmail.com>
2024-09-11 16:23:35 +0530

    initialize app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./pages/Login";
+import Home from "../src/pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center mt-64">
+      <h1 className="text-4xl">You are Successfully LoggedIn</h1>
+      <h1 className="text-3xl">Welcome to Home Screen </h1>
+    </div>
+  );
+}
+
+export default Home;
